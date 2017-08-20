42625dd1561ca1372efa1772b0b027fee7c13b9b
Hardik Bansal <hardikbansal24@gmail.com>
2017-08-20 20:42:15 +0530

    First commit forthe bandit

---
+++ bandit.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+import numpy as numpy
+
+
+# Defining variables
+
+e = 0.1
+
+# Here we will be learning the Policy gradient method
+
+# Loss = log(pi)*A
+# where pi is the weight of that action
+# and A is the reward for that action above the baseline
+
+
+bandits = [0.2 0.8 -0.2 0.6]
+
+# We would like to generate the reward that will be more for 
+# more positive number. So, out function can be a random function
+# that will return 1 with the prob corresponding to the number it is.
+
+# So, what we can do it take a random number in between -1,1 and return 1 if the number 
+# is greater than corresponding bandit and -1 otherwise.
+
+def reward_bandit(index):
+
+	temp = np.random.uniform(-1, 1)
+
+	if(temp > bandits[index]):
+		return -1
+	else:
+		return 1
+
+# So, now that we have defined the reward function. We can define the 
+# tensorflow graph.
+
+
+weights = tf.get_variable("weights",[len(bandits)])
+sugg_move = tf.argmax(weights,0)
+curr_move = tf.placeholder(shape=[1],dtype=tf.int32)
+curr_reward = tf.placeholder(shape=[1],dtype=tf.float32)
+
+
+loss = -tf.log(weights[curr_move])*curr_reward
+
+optimizer = tf.train.AdamOptimizer(0.001, beta1=0.5)
+loss_optimizer = optimizer.minimize(loss)
+
+
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+
+	sess.run(init)
+
+	for step in range(max_steps):
+
+		temp = np.random.random(1)
+
+		if temp > e :
+			action = sess.run([sugg_move], feed_dict={})
+		else :
+			action = np.random.randint(len(bandits), size=1)
+
+		reward = reward_bandit(action)
+
+		sess.run([loss_optimizer], feed_dict={curr_move:action , curr_reward:reward})
+
+
+
