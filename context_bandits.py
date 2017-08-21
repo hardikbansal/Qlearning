4235d06820e04c63e376fbebe10b4f25425b7f55
Hardik Bansal <hardikbansal24@gmail.com>
2017-08-21 22:28:34 +0530

    Nearly Done with the code

--- context_bandits.py
+++ context_bandits.py
@@ -11,6 +11,51 @@ class  contextual_bandits(object):
 		self.state = 0
 		self.bandits = []
 
+		self.num_bandits = len(self.bandits)
+		self.e = 0.1
+
 	def get_reward(self, state, index):
 
+		temp = np.random.uniform(-1, 1)
+
+		if(temp > bandits[state,index]):
+			return -1
+		else :
+			return 1
+
+	def model_setup():
+
+		input_state = tf.placeholder([self.num_bandits], tf.int32)
+
+		weights = tf.Variable(tf.ones([self.num_bandits, self.num_hands]))
+
+		output_weights = tf.matmul(input_state,weights)
+		output_move = tf.max(output_weights)
+
+	def loss_setup():
+
 		
+
+
+	def train():
+
+		init = tf.global_variables_initializer()
+
+		with tf.Session() as sess:
+
+			sess.run(init)
+
+
+			for step in range(num_episodes):
+
+				state = np.random.randint(self.num_bandits, size=1)[0]
+
+				state_oh = np.eyes(self.num_bandits)[state]
+
+				temp = np.random.uniform(0,1)
+
+				if temp > e:
+					action = sess.run([output_move], feed_dict={input_state = state})
+				else:
+					action = np.random.randint(self.num_hands,size=1)
+
