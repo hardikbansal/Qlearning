adfc6d12e8dceb2ff5cd1a31614eb81bbc6991fe
Hardik Bansal <hardikbansal24@gmail.com>
2017-08-23 21:19:50 +0530

    Finally done

--- context_bandits.py
+++ context_bandits.py
@@ -19,27 +19,27 @@ class  contextual_bandits():
 
 		temp = np.random.uniform(-1, 1)
 
-		if(temp > bandits[state,index]):
+		if(temp > self.bandits[state][index]):
 			return -1
 		else :
 			return 1
 
 	def model_setup(self):
 
-		input_state = tf.placeholder(shape=[self.num_bandits], dtype=tf.int32)
+		self.input_state = tf.placeholder(shape=[self.num_bandits], dtype=tf.int32)
 
 		self.weights = tf.Variable(tf.ones([self.num_bandits, self.num_hands]))
 
-		output_weights = tf.matmul(tf.reshape(tf.cast(input_state, dtype=tf.float32),[1,-1]),self.weights)
-		output_move = tf.argmax(output_weights,1)
+		output_weights = tf.matmul(tf.reshape(tf.cast(self.input_state, dtype=tf.float32),[1,-1]),self.weights)
+		self.output_move = tf.argmax(output_weights,1)
 
 	def loss_setup(self):
 
-		curr_move = tf.placeholder(shape=[1],dtype=tf.int32)
-		curr_reward = tf.placeholder(shape=[1],dtype=tf.float32)
-		curr_state = tf.placeholder(shape=[1],dtype=tf.int32)
+		self.curr_move = tf.placeholder(shape=[1],dtype=tf.int32)
+		self.curr_reward = tf.placeholder(shape=[1],dtype=tf.float32)
+		self.curr_state = tf.placeholder(shape=[1],dtype=tf.int32)
 		
-		loss = -tf.log(self.weights[curr_state, curr_move])*curr_reward
+		loss = -tf.log(self.weights[self.curr_state[0], self.curr_move[0]])*self.curr_reward
 
 		optimizer = tf.train.AdamOptimizer(0.001, beta1=0.5)
 		self.loss_optimizer = optimizer.minimize(loss)
@@ -56,20 +56,23 @@ class  contextual_bandits():
 
 			sess.run(init)
 
-			for step in range(num_episodes):
+			for step in range(10000):
 
 				state = np.random.randint(self.num_bandits, size=1)[0]
-				state_oh = np.eyes(self.num_bandits)[state]
+				state_oh = np.eye(self.num_bandits)[state]
 				temp = np.random.uniform(0,1)
-				if temp > e:
-					action = sess.run([output_move], feed_dict={input_state:state})
+				if temp > self.e:
+					action = sess.run(self.output_move, feed_dict={self.input_state:state_oh})
 				else:
+					# action = sess.run(self.output_move, feed_dict={self.input_state:state_oh})
 					action = np.random.randint(self.num_hands,size=1)
 
+				action = action.astype(int)
+				# print(action.shape)
 				reward = self.get_reward(state, action[0])
-				_, temp_weight = sess.run([self.loss_optimizer, self.weights] , feed_dict={curr_state:[state], curr_move:action, curr_reward:reward})
-
-				print(temp_weight)
+				_, temp_weight = sess.run([self.loss_optimizer, self.weights] , feed_dict={self.curr_state:[state], self.curr_move:action, self.curr_reward:[reward]})
+				if(step % 100 == 0):
+					print(temp_weight)
 
 
 def main():
