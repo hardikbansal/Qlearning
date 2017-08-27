5acabee3681730bea3bab06f8ed9abe7a2a15e52
Hardik Bansal <hardikbansal24@gmail.com>
2017-08-28 00:30:15 +0530

    Nearly done with the code

--- cartpole.py
+++ cartpole.py
@@ -6,6 +6,7 @@
 
 import tensorflow as tf
 import numpy as np
+import sys
 
 from layers import *
 
@@ -20,28 +21,41 @@ class cartpole():
 	def __init__(self):
 
 		self.state = 0
+
+		# State size os 4 correspodong tp theta, accn, distance, velocity
 		self.state_size = 4
+
+		# An action denote the force of magnitude 1 in the left or right direction corresponding to 0/1
 		self.action_size = 2
+		self.max_iter = 99
+		self.num_episodes = 1000
+		self.e = 0.1
 
 
 	def model_setup(self):
 
+		# Here we suppose that the network is a fully connected layer
+
 		self.state_in = tf.placeholder(dtype=tf.int32, shape=[None, self.state_size])
-		temp_states = linear1d(self.state_in, self.state_size, self.action_size)
-		self.action = tf.argmax(temp_states)
+		self.prob_action = linear1d(tf.cast(self.state_in, tf.float32), self.state_size, self.action_size)
+		
+		# Taking the action based on the weights
+
+		self.action = tf.argmax(self.prob_action, 1)
 
 
 	def loss_setup(self):
 
-		self.action_hist = tf.placeholder(dtype=tf.int32, shape=[None])
-		self.reward_hist = tf.placeholder(dtype=tf.float32, shape=[None])
+		action_hist = tf.placeholder(dtype=tf.int32, shape=[None,self.action_size])
+		reward_hist = tf.placeholder(dtype=tf.float32, shape=[None])
 
-		self.loss = tf.reduce_mean(tf.loss())
+		# Caclulation the temp weight by taking the weights corresponding action that we got earlier in the stage
 
+		temp_weights = tf.reduce_sum(self.prob_action*tf.cast(action_hist, tf.float32), 1)
 
+		self.loss = tf.reduce_mean(-tf.log(temp_weights)*reward_hist)
 
-
-		return 1
+		# Calculating the gradients in tensorflow
 
 
 
@@ -57,25 +71,39 @@ class cartpole():
 
 			sess.run(init)
 
-			for i in range(episodes):
+			for i in range(self.num_episodes):
 
-				for j in range(max_iter):
+				history = np.array([])
+				curr_state = env.reset()
+
+				for j in range(self.max_iter):
 
 					temp = np.random.uniform(1)
 
-					if temp > e :
-						temp_action = sess.run(self.action, feed_dict={self.state_in:curr_state})
+					if temp > self.e :
+						temp_action = sess.run(self.action, feed_dict={self.state_in:np.reshape(curr_state,[-1, self.state_size])})
 					else :
-						temp_action = np.random.randint(2, size=[1])
+						temp_action = np.random.randint(self.action_size, size=[1])
 
-					news_state, reward, done, _ = env.step(temp_action[0])
+					new_state, reward, done, _ = env.step(temp_action[0])
+					history = np.append(history, np.array([temp_action, curr_state, new_state, reward]))
+
+
+					print(history.shape)
+
+					sys.exit()
+
+					# Here I am applying the gradients after some fixed number of steps.
+
+
+					curr_state = new_state
 
 					if(done):
 						break
 
 def main():
 
-	model = contextual_bandits()
+	model = cartpole()
 	model.train()
 
 if __name__ == "__main__":
