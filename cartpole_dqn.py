336ec8a1c3b460c913fab0bf131b88e64ae356d5
Hardik Bansal <hardikbansal24@gmail.com>
2017-09-03 19:25:14 +0530

    Added simplest network

--- cartpole_dqn.py
+++ cartpole_dqn.py
@@ -23,10 +23,14 @@ class network():
 		with tf.variable_scope(self.name) as scope:
 
 			self.input_state = tf.placeholder(tf.float32, [-1, self.state_size], name="input_state")
-			weight_mat = tf.Variable(tf.ones(self.state_size, self.action_size),dtype=tf.float32)
-			output_weights = tf.nn.sigmoid(tf.matmul(self.input_state, weight_mat))
+			weight_action = tf.Variable(tf.ones(self.state_size, self.action_size),dtype=tf.float32, name="weight_action")
+			weight_reward = tf.Variable(tf.ones(self.state_size, 1),dtype=tf.float32, name="weight_reward")
+			weight_done = tf.Variable(tf.ones(self.state_size, 1),dtype=tf.float32, name="weight_done")
+			output_weights = tf.nn.sigmoid(tf.matmul(self.input_state, weight_action))
 
 			self.out_action = tf.argmax(self.output_weights, 1)
+			self.out_reward = tf.matmul(self.input_state, weight_reward)
+			self.out_done = tf.matmul(self.input_state, weight_done)
 
 
 
@@ -36,6 +40,15 @@ class dqn():
 
 		self.state_size = state_size
 		self.action_size = action_size
+		self.eps = 0.1
+
+	def copy_network(self, net1, net2):
+
+		vars1 = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, net1.name)
+		vars2 = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, net2.name)
+
+		for idx in range(len(vars1)):
+			vars2[idx].assign(val).eval()
 
 	def model(self):
 
@@ -48,6 +61,21 @@ class dqn():
 
 		#Equating the wo networks in the start
 
+		copy_network(self.main_net, self.target_net)
+
+
 	def train(self):
 
-		
+		self.model()
+		init = tf.global_variables_initializer()
+
+		s = env.reset()
+
+		with tf.Session() as sess:
+
+			for i in range(num_episodes):
+
+				temp = np.random.random()
+				
+				if(temp > self.eps):
+					
