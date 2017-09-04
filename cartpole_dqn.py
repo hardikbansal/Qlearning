0f8b84817e80eee79fb03440f4aa766d364a864c
Hardik Bansal <hardikbansal24@gmail.com>
2017-09-04 23:59:10 +0530

    Working cartpole dqn

--- cartpole_dqn.py
+++ cartpole_dqn.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import sys
+import random
 
 from layers import *
 
@@ -46,9 +47,11 @@ class dqn():
 
 		self.num_episodes = 1000
 		self.max_steps = 200
-		self.pre_train_steps = 1000
+		self.pre_train_steps = 20
+		self.update_freq = 10
 		self.batch_size = 20
 		self.gamma = 0.99
+		self.lr = 0.001
 
 	def copy_network(self, net1, net2):
 
@@ -70,14 +73,14 @@ class dqn():
 		# Defining the model for the training
 
 		self.target_reward = tf.placeholder(tf.float32, [None, 1], name="target_reward")
-		self.action_list = tf.placeholder(tf.float32, [None, 1], name="action_list")
+		self.action_list = tf.placeholder(tf.int32, [None, 1], name="action_list")
 
-		observed_reward = self.main_net.output_weights*tf.one_hot(self.action_list)
+		observed_reward = tf.reduce_sum(self.main_net.output_weights*tf.one_hot(self.action_list,2,dtype=tf.float32),1)
 
 		self.loss = tf.reduce_sum(tf.square(self.target_reward - observed_reward))
 
 		optimizer = tf.train.AdamOptimizer(learning_rate=self.lr)
-		self.loss_opt = ptimizer.minimize(self.loss, lr=0.0001)
+		self.loss_opt = optimizer.minimize(self.loss)
 
 
 	def train(self):
@@ -104,8 +107,9 @@ class dqn():
 					if temp < self.eps or total_steps < self.pre_train_steps:
 						temp_action = np.random.randint(self.action_size, size=[1])
 					else :
-						temp_action = sess.run([self.main_net.out_action], feed_dict={self.main_net.input_state:np.reshape(curr_state,[-1, self.state_size])})
+						temp_action = sess.run([self.main_net.out_action[0]], feed_dict={self.main_net.input_state:np.reshape(curr_state,[-1, self.state_size])})
 
+					# print(temp_action.shape)
 					new_state, reward, done, _ = env.step(temp_action[0])
 
 
@@ -121,24 +125,30 @@ class dqn():
 
 						if(total_steps % self.update_freq == 0):
 
-							rand_batch = random.sample(hist_buffer, self.batch_size)
+							rand_batch = hist_buffer[np.random.choice(hist_buffer.shape[0], self.batch_size, replace=False)]
 
 							reward_hist = rand_batch[:,3]
 							state_hist = rand_batch[:,1]
-							action_hist = rand_batch[:,0]
+							action_hist = np.vstack(rand_batch[:,0])
 							next_state_hist = rand_batch[:,2]
 
 							temp_target_q = sess.run(self.main_net.output_weights, feed_dict={self.main_net.input_state:np.vstack(next_state_hist)})
 
+							temp_target_q = np.amax(temp_target_q,1)
 							temp_target_reward = reward_hist + self.gamma*temp_target_q
+							temp_target_reward =  np.reshape(temp_target_reward, [self.batch_size, 1])
+							
+							# print(action_hist.shape)
+
 
 							_ = sess.run(self.loss_opt, feed_dict={self.main_net.input_state:np.vstack(state_hist), self.target_reward:temp_target_reward, self.action_list:action_hist})
 
 							self.copy_network(self.main_net, self.target_net)
+						
+							sys.exit()
 
 
-					if(total_steps == 0):
-						sys.exit()
+					# if(total_steps == 0):
 
 					curr_state = new_state
 
